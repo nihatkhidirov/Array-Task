315705eec01bc5629820e934f312bd2a8f89c0c6
hp <hp@DESKTOP-77U60MC>
2023-02-11 17:44:04 +0400

    Add project files.

---
+++ Array Task/Program.cs
@@ -0,0 +1,130 @@
+﻿namespace Array_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task1
+            //        {
+            //    //        Console.WriteLine("Sira ve Sutundeki Ededlerin Sayini Daxil Edin: ");
+            //    //        int sira = int.Parse(Console.ReadLine());
+            //    //        int sutun = int.Parse(Console.ReadLine());
+            //    //        int[,] matrix = new int[sira, sutun];
+
+
+
+            //    //        Console.WriteLine("Matrixdeki Elementleri Daxil Edin: ");
+            //    //        for (int i = 0; i < sira; i++)
+            //    //        {
+            //    //            for (int d = 0; d < sutun; d++)
+            //    //            {
+            //    //                matrix[i, d] = int.Parse(Console.ReadLine());
+            //    //            }
+            //    //        }
+
+            //    //    }
+            //    //}
+
+            //}
+            #endregion
+
+            #region Task2
+
+            //{
+            //        Console.Write("Massivdeki Elementlerin Sayi: ");
+            //        int n = int.Parse(Console.ReadLine());
+
+            //        int[] array = new int[n];
+
+            //        Console.WriteLine("Massivin Elementlerini Daxil Edin: ");
+            //        for (int i = 0; i < n; i++)
+            //        {
+            //            array[i] = int.Parse(Console.ReadLine());
+            //        }
+
+            //        int say = 0;
+            //        for (int i = 0; i < n; i++)
+            //        {
+            //            if (array[i] % 2 != 0)
+            //            {
+            //                say++;
+            //            }
+            //        }
+
+            //        Console.WriteLine("Massivdeki Tek Ededlerin Sayi: " + say);
+
+            //    }
+            //}
+            //}
+
+            //    }
+            //}
+            #endregion
+
+            #region Task3
+            //    {
+
+
+            //        Console.Write("Massivdeki ELementlerin Sayini Daxil Edin: ");
+            //        int n = int.Parse(Console.ReadLine());
+            //        int[] arr = new int[n];
+            //        int[] arr_1 = new int[n];
+
+            //        Console.WriteLine("Massivdeki Elementleri Daxil Edin: ");
+            //        for (int i = 0; i < n; i++)
+            //        {
+            //            arr[i] = int.Parse(Console.ReadLine());
+            //        }
+
+            //        int minus = arr[1] - arr[0];
+            //        for (int i = 1; i < n - 1; i++)
+            //        {
+            //            int ferq_1 = arr[i + 1] - arr[i];
+            //            if (ferq_1 != minus)
+            //            {
+            //                Console.WriteLine("Silsile Deyildir");
+            //                return;
+            //            }
+            //        }
+            //        Console.WriteLine("Silsiledir");
+            //    }
+            //}
+            //}
+            #endregion
+
+            #region Task4
+            //        {
+
+            //            Console.Write("Massivdeki Elementlerin Sayi: ");
+            //            int n = int.Parse(Console.ReadLine());
+            //            int[] arr = new int[n];
+            //            int[] arr1 = new int[n];
+
+            //            Console.WriteLine("Massivin Elementlerini Daxil Edin: ");
+            //            for (int i = 0; i < n; i++)
+            //            {
+            //                arr[i] = int.Parse(Console.ReadLine());
+            //            }
+
+            //            int c = n - 1;
+            //            for (int i = 0; i < n; i++)
+            //            {
+            //                arr1[i] = arr[c];
+            //                c--;
+            //            }
+
+            //            Console.WriteLine("Tersi: ");
+            //            Console.WriteLine("**************");
+            //            for (int i = 0; i < n; i++)
+            //            {
+            //                Console.WriteLine(arr1[i] + " ");
+            //            }
+
+            //        }
+            //    }
+            //}
+            #endregion
+        }
+
+    }
+}
